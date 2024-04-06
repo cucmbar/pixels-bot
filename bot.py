5b2569cbaee25cb74274e3699b5c24e80aae59e0
bready <none>
2024-04-06 15:15:20 +0300

    a little bug fix in main.py, fixed values on canvas

--- bot.py
+++ bot.py
@@ -1,16 +1,31 @@
 import os
 from dotenv import load_dotenv
-
 import asyncio
-import telegram
-
+from telegram import Update
+import logging
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 load_dotenv()
 
-async def main():
-    bot = telegram.Bot(os.getenv('TOKEN'))
-    async with bot:
-        print(await bot.get_me())
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a monnster, please kill me!")
+
+async def kill(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text='you killed me!')
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(os.getenv('TOKEN')).build()
+    
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+    
+    kill_handler = CommandHandler('kill', kill)
+    application.add_handler(kill_handler)
+    
+    application.run_polling()
 
 
-if __name__ ==  '__main__':
-    asyncio.run(main())
